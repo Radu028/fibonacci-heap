143ee4ff33e602ab0ffdde4f64d215e12bb6f9d7
Radu <radupopa028@gmail.com>
2025-06-02 20:18:42 +0300

    feat: add initial implementation of Node structure and main function

--- main.cpp
+++ main.cpp
@@ -10,4 +10,16 @@ struct Node
 
     bool marked{false};
     int degree;
+
+    Node(int key) : key(key) {}
+    Node(int key, std::shared_ptr<Node> &parent) : key(key), parent(parent) {}
+    Node(int key, std::shared_ptr<Node> &parent, std::shared_ptr<Node> &left,
+         std::shared_ptr<Node> &right)
+        : key(key), parent(parent), left(left), right(right) {}
 };
+
+int main()
+{
+    std::shared_ptr min = std::make_shared<Node>(1);
+    return 0;
+}
